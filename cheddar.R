3227de7b4606ad7e28c36de778b44bd1e5c0682a
Lawrence Hudson <quicklizard@googlemail.com>
2012-05-02 13:13:56 +0100

    A few improvements to the homepage

--- cheddar.R
+++ cheddar.R
@@ -1,4 +1,5 @@
-# Writes cheddar.png
+# Four different views of the community of Tuesday Lake, 1984.
+# Plots are written to images/cheddar.png
 options(warn=2)
 library(cheddar)
 data(TL84)
@@ -6,7 +7,7 @@ png('images/cheddar.png', width=550, height=500)
 par(mfrow=c(2,2),
     mar=c(4, 4, 1, 1),   # bottom, left, top, right
     mgp=c(2, 0.5, 0))    # axis title, axis labels and axis line
-PlotWebByLevel(TL84, show.nodes.as='both', cex=2, main='')
+PlotWebByLevel(TL84, show.nodes.as='both', cex=2, main='', y.layout='stagger')
 PlotCircularWeb(TL84, show.nodes.as='both', cex=2, main='')
 PlotNvM(TL84, show.nodes.as='both', cex=2, main='')
 PlotMCvMR(TL84, main='')
