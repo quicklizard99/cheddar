1004e26b0249b9fe495c5b66ed9ff215b51471d5
Lawrence Hudson <quicklizard@googlemail.com>
2012-05-09 09:58:36 +0100

    Back to png. svg doesn't render everywhere and I don't have time to look at a neat fallback

--- cheddar.R
+++ cheddar.R
@@ -1,27 +1,35 @@
 # Creates image for http://quicklizard99.github.com/cheddar/ homepage
 # Four different views of the community of Tuesday Lake, 1984 are written to 
-# images/cheddar.png.
+# images/cheddar.png and images/cheddar.svg.
 
 options(warn=2)
 library(cheddar)
 data(TL84)
-#png('images/cheddar.png', width=550, height=500)
+
+Plots <- function()
+{
+    par(mfrow=c(2,2),
+        mar=c(4, 4, 1, 1),   # bottom, left, top, right
+        mgp=c(2, 0.5, 0), 
+        lwd=0.5)    # axis title, axis labels and axis line
+    PlotWebByLevel(TL84, show.nodes.as='both', cex=2, main='', y.layout='stagger')
+    mtext('a)', side=2, at=par('usr')[4], line=1, las=1)
+
+    PlotCircularWeb(TL84, show.nodes.as='both', cex=2, main='')
+    mtext('b)', side=2, at=par('usr')[4], line=1, las=1)
+
+    PlotNvM(TL84, show.nodes.as='both', cex=2, main='')
+    mtext('c)', side=2, at=par('usr')[4], line=1, las=1)
+
+    PlotMCvMR(TL84, main='')
+    mtext('d)', side=2, at=par('usr')[4], line=1, las=1)
+}
+
+png('images/cheddar.png', width=550, height=500)
+Plots()
+dev.off()
+
 svg('images/cheddar.svg', width=7.7, height=7)
-par(mfrow=c(2,2),
-    mar=c(4, 4, 1, 1),   # bottom, left, top, right
-    mgp=c(2, 0.5, 0), 
-    lwd=0.5)    # axis title, axis labels and axis line
-PlotWebByLevel(TL84, show.nodes.as='both', cex=2, main='', y.layout='stagger')
-mtext('a)', side=2, at=par('usr')[4], line=1, las=1)
-
-PlotCircularWeb(TL84, show.nodes.as='both', cex=2, main='')
-mtext('b)', side=2, at=par('usr')[4], line=1, las=1)
-
-PlotNvM(TL84, show.nodes.as='both', cex=2, main='')
-mtext('c)', side=2, at=par('usr')[4], line=1, las=1)
-
-PlotMCvMR(TL84, main='')
-mtext('d)', side=2, at=par('usr')[4], line=1, las=1)
-
+Plots()
 dev.off()
 
