df6d15bbd5998a5235d0a15545943814cdc1cf78
Lawrence Hudson <quicklizard@googlemail.com>
2014-04-01 17:25:33 +0100

    Reorganise

---
+++ build.sh
@@ -0,0 +1,8 @@
+VERSION=`cat cheddar/DESCRIPTION  | grep Version: | sed s/.*:\ //g`
+echo "Building cheddar ${VERSION}"
+rm -rf build  && \
+    mkdir build && \
+    cd build && \
+    R CMD build --compact-vignettes=gs ../cheddar && \
+    R CMD check --as-cran cheddar_${VERSION}.tar.gz && \
+    R CMD INSTALL --build --html cheddar_${VERSION}.tar.gz
