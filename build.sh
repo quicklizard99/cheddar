95ce46c9905b4d119eed54e8043bdf46aa57936c
Lawrence Hudson <quicklizard@googlemail.com>
2015-06-13 08:28:24 +0100

    Reorganisation for coveralls

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-VERSION=`cat cheddar/DESCRIPTION  | grep Version: | sed s/.*:\ //g`
+VERSION=`cat DESCRIPTION  | grep Version: | sed s/.*:\ //g`
 echo Building cheddar ${VERSION} using `R --version | head -1`
 rm -rf build  && \
     mkdir build && \
     cd build && \
-    R CMD build --compact-vignettes=gs ../cheddar && \
+    R CMD build --compact-vignettes=gs .. && \
     R CMD check --as-cran cheddar_${VERSION}.tar.gz && \
     R CMD INSTALL --build --html cheddar_${VERSION}.tar.gz
